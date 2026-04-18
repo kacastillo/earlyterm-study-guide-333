6838fc78b227fb7ead2117a548c51833257c78d3
kelley <kelleycastillo@outlook.com>
2026-04-18 10:23:25 -0700

    test testMaxDiffAllPositive

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -17,6 +17,18 @@ public class PracticeTest {
         // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
         assertEquals(14, actual);
     }
+
+     @Test
+    void testMaxDiffAllPositive() {
+        // Arrange
+        int[] numbers = {1, 3, 7, 2};
+
+        // Act
+        int actual = Practice.maxDiff(numbers);
+
+        // Assert — largest: 7, smallest: 1, diff: 6
+        assertEquals(6, actual);
+    }
     
 
     // TODO: Make tests for each problem you solve
